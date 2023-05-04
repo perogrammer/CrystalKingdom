43c6af892735981877a71e19f113f1f98c83b584
Muaz <maazelahi119@gmail.com>
2023-05-04 21:13:46 +0500

    initial commit

---
+++ AVL.h
@@ -0,0 +1,7 @@
+class AVL_Tree {
+	struct Node {
+		int id,reward_score,count;
+		Node* right, * left;
+	};
+	Node* root;
+};
---
+++ Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+#include "AVL.h"
+#include "Dijkstra.h"
+#include "FLoyd.h"
+#include "Prim.h"
+#include "Kruskal.h"
+#include "Game.h"
+int main() {
+    ifstream file("map.txt");
+    if (!file.is_open()) {
+        cout << "Cannot open file!!";
+        return 1;
+    }
+    string map[20],row;
+    int i = 0;
+    while (getline(file, row)) {
+        map[i] = row;
+        i++;
+    }
+    for (auto i : map)
+        cout << i << endl;
+}
